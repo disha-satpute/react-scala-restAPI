2f51b1a7bc31b96ba0ce077381f1f87e465b9849
aditya <adityakotkar47@gmail.com>
2026-02-12 21:15:30 +0530

    feat: implement CORS configuration for UserRoutes in MainApp

--- backend/src/main/scala/MainApp.scala
+++ backend/src/main/scala/MainApp.scala
@@ -1,11 +1,21 @@
 import zio._
+import zio.http.Header.{AccessControlAllowOrigin, Origin}
+import zio.http.Middleware.{CorsConfig, cors}
 import zio.http._
 import routes.UserRoutes
 
 object MainApp extends ZIOAppDefault {
 
-  override def run =
-    Server.serve(UserRoutes.routes.toHttpApp)
+  private val config: CorsConfig =
+    CorsConfig(
+      allowedOrigin = {
+        case origin if origin == Origin.parse("http://localhost:5173").toOption.get =>
+          Some(AccessControlAllowOrigin.Specific(origin))
+        case _ => None
+      },
+    )
 
+  override def run =
+    Server.serve((UserRoutes.routes @@ cors(config)).toHttpApp)
       .provide(Server.defaultWithPort(8080))
 }
